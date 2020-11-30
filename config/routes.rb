055faadccf165a0b11abbdb96d913bc75073b4ff
iqbal engku <engkuiqbal@outlook.com>
2020-11-30 19:54:36 +0800

    Commented out Font Awesome, Added Base Nav bar for entire site, and changed root to equipment#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :equipments, only: [:index, :show]
   devise_for :users
-  root to: 'pages#home'
+  root to: 'equipments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
