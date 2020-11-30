4261420fd9a3e6fb22b04e3137cad23000ba150a
Rupert Yan <yancxr@gmail.com>
2020-11-30 16:49:36 +0800

    Equipment Routes Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'equipments/index'
-  get 'equipments/show'
+  resources :equipments, only: [:index, :show]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
