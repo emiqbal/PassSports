54f43a13cc9bfb6a8890d450d0eabf829a37de98
JToong <toongjosh@gmail.com>
2020-12-01 17:21:21 +0800

    Cloudinary and show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require "open-uri"
 
 Equipment.destroy_all
 
 a = (20..100).to_a
 
-5.times do
+links = [
+  "https://source.unsplash.com/H5aZzaiW_xk",
+  "https://source.unsplash.com/27_ARYvu9YY",
+  "https://source.unsplash.com/UVxd5b-_tw8",
+  "https://source.unsplash.com/DIGO6xmZ5jg",
+  "https://source.unsplash.com/FpYX2jXImNk",
+]
+# links[n]
+
+5.times do |n|
   puts "Creating Equipment using seed..."
   equipment = Equipment.new(name: Faker::Job.title,
     description: Faker::Job.field,
@@ -20,4 +30,8 @@ a = (20..100).to_a
     user: User.first
     )
   equipment.save!
+
+  file = URI.open(links[n])
+  equipment.photo.attach(io: file, filename: "0000#{n}.png", content_type: 'image/png')
+
 end
