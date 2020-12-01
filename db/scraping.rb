911e8b2768cf67e9caeb0654d9a6753059f1cf9b
iqbal engku <engkuiqbal@outlook.com>
2020-12-01 15:21:55 +0800

    Scraping WIP

--- db/scraping.rb
+++ db/scraping.rb
@@ -3,12 +3,31 @@ require 'open-uri'
 
 def scraping_title
 
-  product_title = []
+    product_title = []
+  product_description = []
 
-  doc = Nokogiri::HTML(File.open("db/DECATHLON.html"))
+  doc = Nokogiri::HTML(File.open("DECATHLON.html"))
 
   doc.search('.h3.product-title').each do |element|
-     product_title << element.text.strip.downcase.capitalize
+    product_title << element.text.strip.downcase.capitalize
+
+    description_url = element.search('a').attribute('href').value
+
+    html_file = open(description_url).read
+    html_doc = Nokogiri::HTML(html_file)
+
+    html_doc.search('.product-information').each do |e|
+    product_description << e.search('.rte-content').first.text
+    end
+    break
   end
-  product_title
+# pp product_title
+# pp product_description
+
+product_hash = []
+
+product_title.each_with_index do |product, index|
+  product_hash << { name: product, description: product_description[index] }
+  binding.pry
+end
 end
