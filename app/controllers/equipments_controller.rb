5edcc1ce4799fb338be545cc9575598dbf11eb7c
Rupert Yan <yancxr@gmail.com>
2020-12-01 16:01:49 +0800

    Added simple form and updated controller with new and create

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -6,4 +6,21 @@ class EquipmentsController < ApplicationController
   def show
     @equipment = Equipment.find(params[:id])
   end
+
+  def new
+    @user = current_user
+    @equipment = Equipment.new
+  end
+
+  def create
+    @equipment = Equipment.new(params[:equipment])
+    @equipment.save
+    redirect_to equipment_path(@equipment)
+  end
+
+  private
+
+  def equipment_params
+    params.require(:equipment).permit(:name, :description, :price_per_day)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :equipments, only: [:index, :show, :create]
+  resources :equipments, only: [:index, :show, :create, :new]
   devise_for :users
   root to: 'equipments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
