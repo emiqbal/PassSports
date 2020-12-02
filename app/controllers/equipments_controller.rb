1f610226ff0adc87dc96beb2a1ffd3a9a36b2542
Muhammad Syakiran <syakiranmuhd@gmail.com>
2020-12-02 15:27:08 +0800

    finished default image tag

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -23,16 +23,19 @@ end
     @equipment.user = current_user
     @equipment.price_per_day *= 100
     @equipment.save
+    if equipment_params[:photo].nil?
     query = @equipment.name.split(' ').join(',')
     url = "https://source.unsplash.com/720x480/?#{query}"
     image = URI.open(url)
     @equipment.photo.attach(io: image, filename: "equipment_#{@equipment.id}.jpeg", content_type: 'image/jpeg')
+      # use unsplash
+    end
     redirect_to equipment_path(@equipment)
   end
 
   private
 
   def equipment_params
-    params.require(:equipment).permit(:name, :description, :price_per_day)
+    params.require(:equipment).permit(:name, :description, :price_per_day, :photo)
   end
 end
