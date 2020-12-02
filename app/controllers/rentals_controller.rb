de755d264057066033cd799835267f2b06570bac
Rupert Yan <yancxr@gmail.com>
2020-12-02 12:20:12 +0800

    waiting for datepicker info

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -9,8 +9,8 @@ class RentalsController < ApplicationController
   end
 
   def create
-    # @rental = Rental.new(rental_params)
-    # @rental.user = current_user
-    # rental,price_per_day * (end_date - start_date)
+    @rental = Rental.new(rental_params)
+    @rental.user = current_user
+    #rental,price_per_day * (end_date - start_date)
   end
 end
