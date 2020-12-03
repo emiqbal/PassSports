0e865fe9107c1f60612d493547ebe54abc88a288
Muhammad Syakiran <syakiranmuhd@gmail.com>
2020-12-03 15:55:08 +0800

    added avatar picture feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :photo])
   end
 end
