ebdfd094b747d277afb1563841387ee489612a3e
iqbal engku <engkuiqbal@outlook.com>
2021-01-18 11:36:48 +0800

    Tidy up application and equipments controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username, photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username, photo])
   end
 end
